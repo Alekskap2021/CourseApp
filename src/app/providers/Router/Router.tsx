75029a678b8f1056ae10c16d71e0be68deab21b2
Alekskap2021 <trololo1009@gmail.com>
2024-11-11 18:15:04 +0700

    Remove usless Suspense

--- src/app/providers/Router/Router.tsx
+++ src/app/providers/Router/Router.tsx
@@ -1,20 +1,23 @@
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import { PageLoader } from "@/widgets/PageLoader";
 import { routerConfig } from "./routerConfig";
 
 export const Router = () => {
   return (
-    <Suspense fallback={"loading"}>
-      <Routes>
-        {Object.values(routerConfig).map(({ path, element, ...otherProps }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<section className="pageWrapper">{element}</section>}
-            {...otherProps}
-          />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {Object.values(routerConfig).map(({ path, element, ...otherProps }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <section className="pageWrapper">{element}</section>{" "}
+            </Suspense>
+          }
+          {...otherProps}
+        />
+      ))}
+    </Routes>
   );
 };
