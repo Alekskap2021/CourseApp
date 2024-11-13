a0a6bc834c5897d619c94095164d91968173ab3d
Alekskap2021 <trololo1009@gmail.com>
2024-11-13 21:58:18 +0700

    Added bundle analyzer

--- configs/webpack/buildPlugins.ts
+++ configs/webpack/buildPlugins.ts
@@ -2,9 +2,10 @@ import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import type { Configuration } from "webpack";
 import { ProgressPlugin, DefinePlugin } from "webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import type { BuildOptions } from "./types/BuildOptions";
 
-export function buildPlugins({ paths, isDev }: BuildOptions): Configuration["plugins"] {
+export function buildPlugins({ paths, env }: BuildOptions): Configuration["plugins"] {
   return [
     new ProgressPlugin(),
     new HTMLWebpackPlugin({ template: paths.html }),
@@ -13,7 +14,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): Configuration["plu
       chunkFilename: "css/[name].[contenthash:8].css",
     }),
     new DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev),
+      __IS_DEV__: JSON.stringify(env.mode === "development"),
     }),
-  ];
+    env.analyze && new BundleAnalyzerPlugin({ openAnalyzer: true }),
+  ].filter(Boolean);
 }
