59bfb276366164b0daf03e5e1b9c641f7016e130
Alekskap2021 <trololo1009@gmail.com>
2024-11-19 17:49:41 +0700

    Fixed pipline

--- src/features/Auth/AuthByUsername/ui/AuthByUsername.tsx
+++ src/features/Auth/AuthByUsername/ui/AuthByUsername.tsx
@@ -1,10 +1,6 @@
-import classNames from "classnames/bind";
 import type { ComponentProps, FC } from "react";
 import { Modal } from "@/shared/ui/Modal/Modal";
-import s from "./AuthByUsername.module.scss";
 import { AuthByUsernameForm } from "./AuthByUsernameForm/AuthByUsernameForm";
-
-const cn = classNames.bind(s);
 
 interface AuthByUsernameProps extends Omit<ComponentProps<typeof Modal>, "children"> {
   className?: string;
@@ -14,7 +10,7 @@ export const AuthByUsername: FC<AuthByUsernameProps> = (props) => {
   const { className, ...modalProps } = props;
 
   return (
-    <Modal className={cn(s.AuthByUsername, className)} lazy {...modalProps}>
+    <Modal className={className} lazy {...modalProps}>
       <AuthByUsernameForm />
     </Modal>
   );
