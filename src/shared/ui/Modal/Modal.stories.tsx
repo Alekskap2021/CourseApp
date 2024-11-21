3748065ec6e55e88a7a1513493809e10b5b8d594
Alekskap2021 <trololo1009@gmail.com>
2024-11-21 14:15:43 +0700

    WIP: fix snapshots

--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -15,7 +15,6 @@ const Template: ComponentStory<typeof Modal> = (args) => <Modal {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   isOpen: true,
-  className: "dark",
   children:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ",
 };
@@ -23,15 +22,8 @@ Primary.args = {
 export const Dark = Template.bind({});
 Dark.args = {
   isOpen: true,
+  className: "dark",
   children:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ",
 };
 Dark.decorators = [ThemeDecorator("dark")];
-
-// export const Dark = Template.bind({});
-// Dark.args = {
-//   isOpen: true,
-//   children:
-//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ",
-// };
-// Dark.decorators = [ThemeDecorator("dark")];
