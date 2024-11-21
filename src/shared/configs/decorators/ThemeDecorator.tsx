52e2f94dcd837516d20d5a96acfbd155aed32e73
Alekskap2021 <trololo1009@gmail.com>
2024-11-21 15:32:57 +0700

    WIP: fix snapshots

--- src/shared/configs/decorators/ThemeDecorator.tsx
+++ src/shared/configs/decorators/ThemeDecorator.tsx
@@ -4,13 +4,9 @@ import type { Theme } from "../../types/theme";
 
 // eslint-disable-next-line react/display-name
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => {
-  // eslint-disable-next-line no-console
-  console.log(document.body.classList);
   return (
     <ThemeProvider initialTheme={theme}>
-      <div className={`app ${theme}`}>
-        <StoryComponent />
-      </div>
+      <StoryComponent />
     </ThemeProvider>
   );
 };
--- src/shared/ui/Modal/Modal.stories.tsx
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -23,12 +23,7 @@ Primary.args = {
 export const Dark = Template.bind({});
 Dark.args = {
   isOpen: true,
-  className: "dark",
   children:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ",
-};
-Dark.play = async ({ args, canvasElement }) => {
-  console.log("🚀 ~ Dark.play= ~ canvasElement:", canvasElement.innerHTML);
-  console.log("🚀 ~ Dark.play= ~ args:", args);
 };
 Dark.decorators = [ThemeDecorator("dark")];
